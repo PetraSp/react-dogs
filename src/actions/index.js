5be7afc91324b0339264d85cbb4411a386998516
Petra Spirkova <pspirkova@gmail.com>
2019-08-03 17:13:07 +0200

    create action creator and call it from component

---
+++ src/actions/index.js
@@ -0,0 +1,5 @@
+export const fetchBreeds = () => {
+    return {
+        type: 'FETCH_BREEDS'
+    };
+};
--- src/components/BreedList.js
+++ src/components/BreedList.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchBreeds } from '../actions';
 
 class BreedList extends React.Component {
+    componentDidMount() {
+        this.props.fetchBreeds()
+    }
+
     render(){
         return <div>BreedList</div>
     }
 }
 
-export default BreedList;
+export default connect(
+    null,
+    {fetchBreeds}
+) (BreedList);
