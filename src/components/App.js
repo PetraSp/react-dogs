aaa5006d560701570fbe93c4ce70069dca4ebd3c
Petra Spirkova <pspirkova@gmail.com>
2019-08-03 16:54:55 +0200

    create BreedList component, add semantic library

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import BreedList from './BreedList';
 
 const App = () => {
-    return <div>App</div>
+    return (
+        <div className="ui container">
+            <BreedList />
+        </div>
+    );
 };
 
 export default App;
---
+++ src/components/BreedList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class BreedList extends React.Component {
+    render(){
+        return <div>BreedList</div>
+    }
+}
+
+export default BreedList;
